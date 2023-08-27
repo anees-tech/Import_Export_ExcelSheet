19de6e9bd8dd4fb43c805ce11d607d1bffbb0ff1
anees-tech <aneesliaqat555@gmail.com>
2023-08-27 22:43:07 +0500

    all changes done

--- src/layouts/ImportCSVdata.jsx
+++ src/layouts/ImportCSVdata.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
+import Papa from "papaparse";
 
-function ImportCSVdata() {
+function ImportCSVButton({ onImport }) {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+    if (selectedFile) {
+      Papa.parse(selectedFile, {
+        complete: (result) => {
+          onImport(result.data);
+          // console.log(result.data);
+        },
+      });
+    }
+  };
+
   return (
     <>
-        <button className="bg-gray-500 border-solid border-2 border-gray-500 hover:bg-white hover:text-gray-400 text-white font-bold py-2 px-4 rounded">Add CSV</button>
+      <label htmlFor="csv" className="bg-gray-500 border-solid border-2 border-gray-500 hover:bg-white hover:text-gray-400 text-white font-bold py-2 px-4 rounded">
+        Import CSV
+      </label>
+      <input id="csv" className="hidden" type="file" accept=".csv" onChange={handleFileChange} />
+      {/* {file && <p>File uploaded: {file.name}</p>} */}
     </>
-  )
+  );
 }
 
-export default ImportCSVdata
+export default ImportCSVButton;
--- src/layouts/Table.jsx
+++ src/layouts/Table.jsx
@@ -1,49 +1,60 @@
+import { useState } from 'react';
 import ExportCSVdata from './ExportCSVdata';
 import ImportCSVdata from './ImportCSVdata';
 import Sidebar from './Sidebar'
-const TABLE_HEAD = ["Name", "Job", "Employed", ""];
+const TABLE_HEAD = ["Name", "Job", "Employed", "id"];
 
-const TABLE_ROWS = [
+let tableData = [
   {
     name: "Arham ALI",
     job: "Manager",
     date: "23/04/18",
-    id:1,
+    id: 1,
   },
   {
     name: "Arham ALI",
     job: "Manager",
     date: "23/04/18",
-    id:2,
-  },  {
+    id: 2,
+  }, {
     name: "Arham ALI",
     job: "Manager",
     date: "23/04/18",
-    id:3,
-  },  {
+    id: 3,
+  }, {
     name: "Arham ALI",
     job: "Manager",
     date: "23/04/18",
-    id:4,
-  },  {
+    id: 4,
+  }, {
     name: "Arham ALI",
     job: "Manager",
     date: "23/04/18",
-    id:5,
-  }, 
-  
+    id: 5,
+  },
+
 ];
 
+
 function Table() {
+  const [importedData, setImportedData] = useState([])
+
+  const handleImport = (data) => {
+    // tableData = []
+    setImportedData(data);
+    // console.log(data);
+  };
+
+
   return (
     <div className="flex">
       <Sidebar />
       <div className="h-full w-full overflow-scroll flex-1 ml-60 p-6">
-        <div className="flex"> 
+        <div className="flex">
           <h2 className=' text-3xl'>Users</h2>
           <div className="ml-auto">
-            <ImportCSVdata />
-            <ExportCSVdata data={TABLE_ROWS} filename={'exported-Data.csv'} />
+            <ImportCSVdata onImport={handleImport} />
+            <ExportCSVdata data={tableData} filename={'exported-Data.csv'} />
           </div>
         </div>
         <table className="w-full min-w-max table-auto text-left">
@@ -60,29 +71,49 @@ function Table() {
             </tr>
           </thead>
           <tbody>
-            {TABLE_ROWS.map(({ name, job, date, id }, index) => {
-              const isLast = index === TABLE_ROWS.length - 1;
-              const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
+            {importedData.length > 0
+                  ? importedData.map((rowData, rowIndex) => {
+                // Skip the first row (header row)
+                if (rowIndex === 0) return null;
+        
+                const isLast = rowIndex === importedData.length - 1;
+                const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
+        
+                return (
+                  <tr key={rowIndex}>
+                    {rowData.map((item, columnIndex) => (
+                      <td key={columnIndex} className={classes}>
+                        <p className="font-normal">{item}</p>
+                      </td>
+                    ))}
+                  </tr>
+                );
+              })
+        
+              : tableData.map(({ name, job, date, id }, index) => {
+                const isLast = index === tableData.length - 1;
+                const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
 
-              return (
-                <tr key={id}>
-                  <td className={classes}>
-                    <p className="font-normal">{name}</p>
-                  </td>
-                  <td className={classes}>
-                    <p variant="small" color="blue-gray" className="font-normal">
-                      {job}
-                    </p>
-                  </td>
-                  <td className={classes}>
-                    <p variant="small" color="blue-gray" className="font-normal">
-                      {date}
-                    </p>
-                  </td>
-                </tr>
-              );
-            })}
+                return (
+                  <tr key={id}>
+                    <td className={classes}>
+                      <p className="font-normal">{name}</p>
+                    </td>
+                    <td className={classes}>
+                      <p variant="small" color="blue-gray" className="font-normal">
+                        {job}
+                      </p>
+                    </td>
+                    <td className={classes}>
+                      <p variant="small" color="blue-gray" className="font-normal">
+                        {date}
+                      </p>
+                    </td>
+                  </tr>
+                );
+              })}
           </tbody>
+
         </table>
       </div>
     </div>
